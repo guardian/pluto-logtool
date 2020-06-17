3349e0e6af8ea8e1b7e95acbd5f3ac2f89658d7a
Dave Allison <david.allison@theguardian.com>
2020-06-17 13:56:12 +0100

    Messy attempt at Hello World!

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {BrowserRouter, Link, Route, Switch, Redirect, withRouter} from 'react-router-dom';
+//import { library } from '@fortawesome/fontawesome-svg-core'
+//import { faFolder, faFolderOpen, faTimes, faSearch, faCog, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+//import RootComponent from "./RootComponent.jsx";
+//import NotFoundComponent from "./NotFoundComponent.jsx";
+//import OAuthCallbackComponent from "./OAuthCallbackComponent.jsx";
+//import LoginBanner from "./LoginBanner.jsx";
+
+//library.add(faFolder, faFolderOpen, faTimes, faSearch, faCog, faUser, faSignOutAlt);
+
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
