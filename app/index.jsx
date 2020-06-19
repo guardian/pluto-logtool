d3aeb965329a04e1dddfb48c18524da21d18b52b
Dave Allison <david.allison@theguardian.com>
2020-06-19 11:18:28 +0100

    Basic working data load and display from the Vidispine REST API

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {BrowserRouter, Link, Route, Switch, Redirect, withRouter} from 'react-router-dom';
+//import React from 'react';
+//import {render} from 'react-dom';
+//import {BrowserRouter, Link, Route, Switch, Redirect, withRouter} from 'react-router-dom';
 //import { library } from '@fortawesome/fontawesome-svg-core'
 //import { faFolder, faFolderOpen, faTimes, faSearch, faCog, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 //import RootComponent from "./RootComponent.jsx";
@@ -11,7 +11,20 @@ import {BrowserRouter, Link, Route, Switch, Redirect, withRouter} from 'react-ro
 //library.add(faFolder, faFolderOpen, faTimes, faSearch, faCog, faUser, faSignOutAlt);
 
 
-render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('app')
-);
+//render(
+//  <h1>Vidispine Job Tool</h1>,
+//  document.getElementById('app')
+//);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './App.jsx';
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
