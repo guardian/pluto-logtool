29d7bf932ffde40db585d681c8e44e6a6095c437
Dave Allison <david.allison@theguardian.com>
2020-06-25 18:10:14 +0100

    Page sizing, a go to page form, and various other paging improvements

--- app/VidispineJobTool.jsx
+++ app/VidispineJobTool.jsx
@@ -10,8 +10,10 @@ constructor(props){
       hits: 0,
       job: []
     },
-    pageNumber: 1
+    pageNumber: 1,
+    pageSize: 16
   };
+  this.handleSubmit = this.handleSubmit.bind(this);
 }
 
   setStatePromise(newState) {
@@ -42,23 +44,115 @@ constructor(props){
   }
 
   pageHigher = () => {
-    console.log(this.state.pageNumber)
-    this.setState({ pageNumber: this.state.pageNumber + 1 });
-    var pageToLoad = 1;
-    if (this.state.pageNumber > 0) {
-      pageToLoad = this.state.pageNumber * 16 + 1;
-    }
-    this.getJobData('job?metadata=true&step=true&number=16&first=' + pageToLoad + '&sort=jobId%20desc');
+    this.setState({
+      pageNumber: this.state.pageNumber + 1
+    },() => {
+      var placeToLoad = 1;
+      if (this.state.pageNumber > 1) {
+        placeToLoad = this.state.pageNumber * this.state.pageSize - this.state.pageSize + 1;
+      }
+      this.getJobData('job?metadata=true&step=true&number=' + this.state.pageSize + '&first=' + placeToLoad + '&sort=jobId%20desc');
+    });
   }
 
   pageLower = () => {
-    console.log(this.state.pageNumber)
-    this.setState({ pageNumber: this.state.pageNumber - 1 });
-    var pageToLoadTwo = 1;
-    if (this.state.pageNumber > 0) {
-      pageToLoadTwo = this.state.pageNumber * 16 - 31;
+    this.setState({
+      pageNumber: this.state.pageNumber - 1
+    },() => {
+      var placeToLoadTwo = 1;
+      if (this.state.pageNumber > 1) {
+        placeToLoadTwo = this.state.pageNumber * this.state.pageSize - this.state.pageSize + 1;
+      }
+      this.getJobData('job?metadata=true&step=true&number=' + this.state.pageSize + '&first=' + placeToLoadTwo + '&sort=jobId%20desc');
+    });
+  }
+
+  pageSize16 = () => {
+    this.setState({
+      pageSize: 16
+    },() => {
+      var placeToLoad16 = 1;
+      if (this.state.pageNumber > 1) {
+        placeToLoad16 = (this.state.pageNumber * this.state.pageSize) - this.state.pageSize + 1;
+      }
+      this.getJobData('job?metadata=true&step=true&number=16&first=' + placeToLoad16 + '&sort=jobId%20desc');
+    });
+  }
+
+  pageSize32 = () => {
+    this.setState({
+      pageSize: 32
+    },() => {
+      var placeToLoad32 = 1;
+      if (this.state.pageNumber > 1) {
+        placeToLoad32 = (this.state.pageNumber * this.state.pageSize) - this.state.pageSize + 1;
+      }
+      this.getJobData('job?metadata=true&step=true&number=32&first=' + placeToLoad32 + '&sort=jobId%20desc');
+    });
+  }
+
+  pageSize64 = () => {
+    this.setState({
+      pageSize: 64
+    },() => {
+      var placeToLoad64 = 1;
+      if (this.state.pageNumber > 1) {
+        placeToLoad64 = (this.state.pageNumber * this.state.pageSize) - this.state.pageSize + 1;
+      }
+      this.getJobData('job?metadata=true&step=true&number=64&first=' + placeToLoad64 + '&sort=jobId%20desc');
+    });
+  }
+
+  pageSize128 = () => {
+    this.setState({
+      pageSize: 128
+    },() => {
+      var placeToLoad128 = 1;
+      if (this.state.pageNumber > 1) {
+        placeToLoad128 = (this.state.pageNumber * this.state.pageSize) - this.state.pageSize + 1;
+      }
+      this.getJobData('job?metadata=true&step=true&number=128&first=' + placeToLoad128 + '&sort=jobId%20desc');
+    });
+  }
+
+  placeToShow() {
+    var placeToReturn = 1;
+    if (this.state.pageNumber > 1) {
+      placeToReturn = (this.state.pageNumber * this.state.pageSize) - this.state.pageSize + 1;
     }
-    this.getJobData('job?metadata=true&step=true&number=16&first=' + pageToLoadTwo + '&sort=jobId%20desc');
+    return placeToReturn;
+  }
+
+  placeToShowEnd() {
+    var placeToReturnEnd = this.state.pageSize;
+    if (this.state.pageNumber > 1) {
+      placeToReturnEnd = (this.state.pageNumber * this.state.pageSize) + 1;
+    }
+    if (placeToReturnEnd > this.state.someData.hits) {
+      placeToReturnEnd = this.state.someData.hits;
+    }
+    return placeToReturnEnd;
+  }
+
+  totalPages() {
+    var totalToReturn = 1;
+    totalToReturn = Math.ceil(this.state.someData.hits / this.state.pageSize);
+    return totalToReturn;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    //this.setState({ pageNumber: this.element.value });
+
+    this.setState({
+      pageNumber: parseInt(this.element.value)
+    },() => {
+      var placeToLoadSub = 1;
+      if (this.state.pageNumber > 1) {
+        placeToLoadSub = (this.state.pageNumber * this.state.pageSize) - this.state.pageSize + 1;
+      }
+      this.getJobData('job?metadata=true&step=true&number=' + this.state.pageSize + '&first=' + placeToLoadSub + '&sort=jobId%20desc');
+    });
   }
 
   render() {
@@ -67,11 +161,32 @@ constructor(props){
         <div class="grid">
         <div class="title_box">Vidispine Job Tool</div>
         <div class="controls_box">
+          <div class="size_button_16" onClick={this.pageSize16}>
+            16
+          </div>
+          <div class="size_button_32" onClick={this.pageSize32}>
+            32
+          </div>
+          <div class="size_button_64" onClick={this.pageSize64}>
+            64
+          </div>
+          <div class="size_button_128" onClick={this.pageSize128}>
+            128
+          </div>
           <div class="job_number">
-            {this.state.someData.hits} jobs
+            Showing {this.placeToShow()} to {this.placeToShowEnd()} of {this.state.someData.hits} jobs
+          </div>
+          <div class="middle_placeholder">
+            &nbsp;
           </div>
           <div class="page_number">
-            Page {this.state.pageNumber}
+            Page {this.state.pageNumber} of {this.totalPages()}
+          </div>
+          <div class="page_form">
+            <form onSubmit={this.handleSubmit}>
+              <input size="4" type="text" ref={el => this.element = el} />
+              <input type="submit" value="Go" />
+            </form>
           </div>
           <div class="last_page" onClick={this.pageLower}>
             <div class="arrow_left"></div>
