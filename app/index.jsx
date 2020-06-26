bdef5e2a843ba440e52fbbbbad6ea592af6884a6
Dave Allison <david.allison@theguardian.com>
2020-06-26 17:41:14 +0100

    Use props for Vidispine access

--- app/index.jsx
+++ app/index.jsx
@@ -6,11 +6,19 @@ import JobPage from './JobPage.jsx';
 
 class App extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      username: 'admin',
+      password: 'admin'
+    };
+  }
+
   render(){
           return <div>
               <Switch>
-                  <Route path="/job/:id" component={JobPage}/>
-                  <Route path="/" component={VidispineJobTool}/>
+                  <Route path="/job/:id" component={()=><JobPage vidispine_host="http://localhost:8080" username={this.state.username ?? "admin"} password={this.state.password ?? "admin"} />}/>
+                  <Route path="/" component={()=><VidispineJobTool vidispine_host="http://localhost:8080" username={this.state.username ?? "admin"} password={this.state.password ?? "admin"} />}/>
               </Switch>
           </div>
       }
